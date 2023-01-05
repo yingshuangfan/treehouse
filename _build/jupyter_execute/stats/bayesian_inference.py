12a19fcad6e07c54b0477a466f8d81ed701b3da2
Yingshuang Fan <yingshuang.fan@Yingshuangs-MacBook-Pro.local>
2023-01-05 22:06:11 +0800

    re-commit

--- _build/jupyter_execute/stats/bayesian_inference.py
+++ _build/jupyter_execute/stats/bayesian_inference.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Bayesian Inference
+# # Bayesian Inference (Part-I Parametric)
 # 
 # ## Posterior
 # 
--- _build/jupyter_execute/stats/non-param_bayesian.py
+++ _build/jupyter_execute/stats/non-param_bayesian.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Non-Parametric Bayesian
+# # Bayesian Inference (Part-II Non-Parametric)
 # 
 # ## Gaussian Process
 # 
